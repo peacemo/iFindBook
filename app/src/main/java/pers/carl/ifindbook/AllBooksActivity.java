0152ba5e340dfcf1431320dbc4a42b7a8714dc17
peacemo <1321500713@qq.com>
2021-05-13 14:07:51 +0800

    lil update

--- app/src/main/java/pers/carl/ifindbook/AllBooksActivity.java
+++ app/src/main/java/pers/carl/ifindbook/AllBooksActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowInsets;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
--- app/src/main/java/pers/carl/ifindbook/MainActivity.java
+++ app/src/main/java/pers/carl/ifindbook/MainActivity.java
@@ -49,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
+
         getWindow().getDecorView().setSystemUiVisibility(
                   View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
